6cd6870992eb36222228df9bd97a930b993e0bfb
Sri Kameswari Kotra <71426897+srikameswari@users.noreply.github.com>
2021-06-29 07:53:13 +0530

    printing number of even and odd

---
+++ evenandodd.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main
+
+{
+	
+public static void main(String[] args) 
+{
+	   
+Scanner scan=new Scanner(System.in);
+	   
+int count=0, countO=0, r=0;
+	  
+ int n=scan.nextInt();
+	   
+int i = 0;
+	   
+while(n!=0)
+	  
+ {
+	       
+r = n%10;
+	       
+n/=10;
+	       
+if(r%2==0)
+	       
+{
+	           
+count++;
+	       
+}
+	       
+if(r%2!=0)
+	      
+ {
+	         
+ countO++;
+	       
+}
+	   
+}
+	   
+System.out.println("odd digits="+count);
+	  
+ System.out.println("even digits="+countO);
+	   
+	
+}
+
+}
