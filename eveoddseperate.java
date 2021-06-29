55fe14fb97a2697b036394bec8281c940f80b043
Sri Kameswari Kotra <71426897+srikameswari@users.noreply.github.com>
2021-06-29 21:36:49 +0530

    Printing even and odd seperately in a digit

---
+++ eveoddseperate.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main
+
+{
+	
+public static void main(String[] args) 
+{
+	    
+Scanner scan=new Scanner(System.in);
+	    
+int r=0;
+	    
+int n=scan.nextInt();
+	    
+	   
+while(n!=0)
+	  
+{
+	      
+ r=n%10;
+	      
+ n/=10;
+	       
+if(r%2==0)
+	      
+ {
+	        
+System.out.println("This is even:"+r);   
+	       
+}
+	     
+ if(r%2!=0)
+	      
+{
+	       
+ System.out.println("This is odd:"+r);  
+	       
+}
+	  
+}
+	
+}
+
+}
