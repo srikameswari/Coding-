0aa7c60618bb79c65630b1453deb61519b0f0826
Sri Kameswari Kotra <71426897+srikameswari@users.noreply.github.com>
2021-06-29 21:11:07 +0530

    Reverse integer for +ve number and -ve number also

---
+++ reversesub.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class revsub
+
+{
+	
+public static void main(String[] args) 
+{
+	
+Scanner s=new Scanner(System.in);
+	
+int n= s.nextInt();
+	
+int r,rev=0;
+	
+while(n!=0)
+	
+{
+	    
+r=n%10;
+	    
+rev=rev*10+r;
+	    
+n=n/10;
+	
+}
+	
+	
+System.out.println(rev);
+	
+}
+
+}
