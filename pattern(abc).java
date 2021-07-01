a116fd30d1453fe364c2c5bd68ade69d81d089bc
Sri Kameswari Kotra <71426897+srikameswari@users.noreply.github.com>
2021-07-01 21:02:47 +0530

    Add files via upload

---
+++ pattern(abc).java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main
+
+{
+	
+public static void main(String[] args)
+ {
+	    
+Scanner sc=new Scanner(System.in);
+	    
+int N=sc.nextInt();
+	    
+char ch='A';
+for(int i=1;i<=N;i++)
+	   
+ 
+{
+	   
+         
+
+for(int j=1;j<=i;j++)
+         
+
+{
+           
+  
+System.out.print((ch++)+" ");
+
+}
+       
+  
+System.out.println("");
+     
+
+} 
+	
+
+}
+
+
+}
