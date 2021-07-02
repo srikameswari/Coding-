707ce74a8ed04d6ded19ceb618b94a2f1f8a67e8
Sri Kameswari Kotra <71426897+srikameswari@users.noreply.github.com>
2021-07-02 18:24:45 +0530

    Add files via upload

---
+++ primerange.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main
+
+{
+	
+public static void main(String[] args)
+ {
+		
+int i,count=0,X, count1=0;
+		
+Scanner sc=new Scanner(System.in);
+		
+X=sc.nextInt();
+		
+for(int n=1;n<=X;n++)
+		
+{
+		    
+count=0;
+		    
+for(i=1;i<=n;i++)
+		   
+ {
+		        
+if(n%i==0)
+		      
+ {
+		           
+ count++;
+		       
+ }
+		   
+ }
+		   
+ if(count==2)
+		  
+ {
+		       
+ count1++;
+		    
+}
+		
+}
+		
+System.out.println(count1);
+	
+}	
+
+}
