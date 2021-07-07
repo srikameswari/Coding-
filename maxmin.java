071aacfabe00d759179dbce1c1a36a1c5dcb0f91
Sri Kameswari Kotra <71426897+srikameswari@users.noreply.github.com>
2021-07-07 07:07:56 +0530

    mam and min

---
+++ maxmin.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main
+
+{
+	
+public static void main(String[] args) 
+{
+	    
+Scanner sc=new Scanner(System.in);
+	   
+ int n;
+	   
+ n=sc.nextInt();
+	    
+int a[]=new int[n];
+	   
+ System.out.print("Elements in array: ");
+	    
+for(int j=0;j<n;j++)
+	    
+{
+	    
+a[j]=sc.nextInt();
+	    
+}
+	     
+int max=0,min=a[0];
+
+for(int i=0;i<=n-1;i++)
+
+{
+    
+if(max<a[i])
+    
+{
+        
+max=a[i];
+    
+}
+    
+if(min>a[1])
+    
+{
+        
+min=a[i];
+    
+}
+
+}
+
+System.out.print("min = "+min);
+
+System.out.println(" max = "+max);
+	
+}
+
+}
